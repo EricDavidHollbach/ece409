b0e56a5474c676fddbd4c7204d9c8a4ae5fe9822
Eric <edhollba@edu.uwaterloo.ca>
2018-03-09 13:15:28 -0500

    update == to .equals() and remove bad code

--- Main.java
+++ Main.java
@@ -6,19 +6,18 @@ public class Main {
 
         BigInteger q = new BigInteger("959452661475451209325433595634941112150003865821");
 
+        BigInteger g = new BigInteger("94389192776327398589845326980349814526433869093412782345430946059206568804005181600855825906142967271872548375877738949875812540433223444968461350789461385043775029963900638123183435133537262152973355498432995364505138912569755859623649866375135353179362670798771770711847430626954864269888988371113567502852");
+
         if (p.isProbablePrime(Integer.MAX_VALUE)) {
             System.out.println("p is prime");
         }
 
-        if (q.isProbablePrime(Integer.MAX_VALUE) && p.subtract(BigInteger.ONE).mod(q) == BigInteger.ZERO) {
+        if (q.isProbablePrime(Integer.MAX_VALUE) && p.subtract(BigInteger.ONE).mod(q).equals(BigInteger.ZERO)) {
             System.out.println("q is a prime factor of p - 1");
         }
 
-        for (int i = 1; i < q.intValue(); i++) {
-            if (q.pow(i).mod(p) == BigInteger.ZERO) {
-                System.out.printf("g has order %d%n", i);
-            }
-        }
+        // TODO check if g is order q
+
         return;
     }
 }
