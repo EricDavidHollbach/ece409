41eaf1ec67c3c4709536531df571986a0c743649
Eric <edhollba@edu.uwaterloo.ca>
2018-03-26 18:04:17 -0400

    Finish part (b)

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
 import java.lang.Process;
 import java.lang.ProcessBuilder;
+
 import java.math.BigInteger;
+
 import java.security.SecureRandom;
 
 public class Main
 {
-    private static final BigInteger p = new BigInteger("23");//new BigInteger("168199388701209853920129085113302407023173962717160229197318545484823101018386724351964316301278642143567435810448472465887143222934545154943005714265124445244247988777471773193847131514083030740407543233616696550197643519458134465700691569680905568000063025830089599260400096259430726498683087138415465107499"); 
-    private static final BigInteger q = new BigInteger("11");//new BigInteger("959452661475451209325433595634941112150003865821");
-    private static final BigInteger g = new BigInteger("2");//new BigInteger("94389192776327398589845326980349814526433869093412782345430946059206568804005181600855825906142967271872548375877738949875812540433223444968461350789461385043775029963900638123183435133537262152973355498432995364505138912569755859623649866375135353179362670798771770711847430626954864269888988371113567502852");
+    private static final BigInteger p = new BigInteger("168199388701209853920129085113302407023173962717160229197318545484823101018386724351964316301278642143567435810448472465887143222934545154943005714265124445244247988777471773193847131514083030740407543233616696550197643519458134465700691569680905568000063025830089599260400096259430726498683087138415465107499"); 
+    private static final BigInteger q = new BigInteger("959452661475451209325433595634941112150003865821");
+    private static final BigInteger g = new BigInteger("94389192776327398589845326980349814526433869093412782345430946059206568804005181600855825906142967271872548375877738949875812540433223444968461350789461385043775029963900638123183435133537262152973355498432995364505138912569755859623649866375135353179362670798771770711847430626954864269888988371113567502852");
 
     public static void main(String[] args) {
         if (p.isProbablePrime(Integer.MAX_VALUE)) {
@@ -21,13 +24,13 @@ public class Main
             System.out.println("q is a prime factor of p - 1");
         }
 
-        // TODO use a simple for loop here
+        // After checking all integers less than g
         if (g.modPow(q, p).equals(BigInteger.ONE)) {
             System.out.println("g has order q");
         }
 
-        BigInteger sk1 = new BigInteger("5");//BigInteger.ZERO.setBit(q.bitLength() - 2).subtract(BigInteger.ZERO);
-        BigInteger pk1 = new BigInteger("9");//g.modPow(sk1, p);
+        BigInteger sk1 = new BigInteger("432398415306986194693973996870836079581453988813");
+        BigInteger pk1 = new BigInteger("49336018324808093534733548840411752485726058527829630668967480568854756416567496216294919051910148686186622706869702321664465094703247368646506821015290302480990450130280616929226917246255147063292301724297680683401258636182185599124131170077548450754294083728885075516985144944984920010138492897272069257160");
 
         if (pk1.equals(g.modPow(sk1, p))) {
             System.out.println("(sk1, pk1) is valid");
@@ -57,6 +60,10 @@ public class Main
         // System.out.println(user2.verify(m, sig1));
 
         BigInteger m = new BigInteger(pk1.toString(16) + pk2.toString(16) + "00000001", 16);
+        System.out.println("m = " + m.toString());
+
+        System.out.println("x = " + sk1.toString());
+        System.out.println("y = " + pk1.toString());
 
         DSS test = new DSS(sk1, pk1);
         BigInteger[] sig = test.sign(m);
@@ -77,8 +84,9 @@ public class Main
         protected BigInteger[] sign(BigInteger m) {
             System.out.println("Sig");
 
-            BigInteger h_m = m.mod(p);//new BigInteger(sha3_224(m.toString(16)), 16);
-            System.out.println("1. h_m = " + h_m.toString());
+            BigInteger h_m = new BigInteger(sha3_224(m.toString(16)), 16);
+
+            System.out.println("1. h_m = " + h_m.mod(q).toString());
 
             SecureRandom rnd = new SecureRandom();
             BigInteger k = new BigInteger(q.bitLength(), rnd);
@@ -100,7 +108,7 @@ public class Main
                 return new BigInteger[] {r, s};
             }
 
-            System.out.println("Error generating a valid signature");
+            System.out.println("Error generating a valid signature.");
             return null;
         }
 
@@ -112,19 +120,19 @@ public class Main
 
             for (BigInteger rs : sig) {
                 if (rs.compareTo(q) > 0 || rs.compareTo(BigInteger.ZERO) <= 0) {
-                    System.out.println("Invalid signature -- r or s is not nonnegative or too large");
+                    System.out.println("Reject the signature since 0 < r,s < q is not satisfied.");
                     return false;
                 }
             }
 
-            BigInteger h_m = m.mod(p);//new BigInteger(sha3_224(m.toString(16)), 16);
-            System.out.println("1. h_m = " + h_m.toString());
+            BigInteger h_m = new BigInteger(sha3_224(m.toString(16)), 16);
+            System.out.println("1. h_m = " + h_m.mod(q).toString());
 
             BigInteger r = sig[0];
             BigInteger s = sig[1];
 
             BigInteger s_inv = s.modInverse(q);
-            System.out.println("2. s^-1 = " + s_inv.toString());
+            System.out.println("2. s^{-1} = " + s_inv.toString());
 
             BigInteger u = h_m.multiply(s_inv).mod(q);
             System.out.println("3. u = " + u.toString());
@@ -132,7 +140,7 @@ public class Main
             BigInteger v = r.negate().multiply(s_inv).mod(q);
             System.out.println("4. v = " + v.toString());
 
-            BigInteger w = g.modPow(u, p).multiply(y.modPow(v, p)).mod(q);
+            BigInteger w = g.modPow(u, p).multiply(y.modPow(v, p)).mod(p).mod(q);
             System.out.println("5. w = " + w.toString());
 
             if (w.equals(r)) {
