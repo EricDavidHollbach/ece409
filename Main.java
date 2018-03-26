8b511a286b91bfcbae2b7833c8fca778a423cb3c
Eric <edhollba@edu.uwaterloo.ca>
2018-03-26 18:21:03 -0400

    fix someshit

--- Main.java
+++ Main.java
@@ -16,6 +16,8 @@ public class Main
     private static final BigInteger g = new BigInteger("94389192776327398589845326980349814526433869093412782345430946059206568804005181600855825906142967271872548375877738949875812540433223444968461350789461385043775029963900638123183435133537262152973355498432995364505138912569755859623649866375135353179362670798771770711847430626954864269888988371113567502852");
 
     public static void main(String[] args) {
+        System.out.println("--------------------part (a)--------------------");
+
         if (p.isProbablePrime(Integer.MAX_VALUE)) {
             System.out.println("p is prime");
         }
@@ -24,10 +26,7 @@ public class Main
             System.out.println("q is a prime factor of p - 1");
         }
 
-        // After checking all integers less than g
-        if (g.modPow(q, p).equals(BigInteger.ONE)) {
-            System.out.println("g has order q");
-        }
+        // TODO use MultiplicativeOrder to check whether the order of g under GF(p) is q
 
         BigInteger sk1 = new BigInteger("432398415306986194693973996870836079581453988813");
         BigInteger pk1 = new BigInteger("49336018324808093534733548840411752485726058527829630668967480568854756416567496216294919051910148686186622706869702321664465094703247368646506821015290302480990450130280616929226917246255147063292301724297680683401258636182185599124131170077548450754294083728885075516985144944984920010138492897272069257160");
@@ -50,24 +49,33 @@ public class Main
             System.out.println("(sk3, pk3) is valid");
         }
 
-        // BigInteger m = new BigInteger(pk1.toString(16) + pk2.toString(16) + "00000001", 16);
-
-        // DSS user1 = new DSS(sk1, pk1);
-        // BigInteger[] sig1 = user1.sign(m);
-        // System.out.println(user1.verify(m, sig1));
-
-        // DSS user2 = new DSS(null, pk1);
-        // System.out.println(user2.verify(m, sig1));
+        System.out.println("--------------------part (b)--------------------");
+        System.out.println("User 1 generate Sig1:");
 
         BigInteger m = new BigInteger(pk1.toString(16) + pk2.toString(16) + "00000001", 16);
-        System.out.println("m = " + m.toString());
+        System.out.println("m = 0x" + m.toString(16));
 
-        System.out.println("x = " + sk1.toString());
-        System.out.println("y = " + pk1.toString());
+        System.out.println("x = 0x" + sk1.toString(16));
+        System.out.println("y = 0x" + pk1.toString(16));
 
-        DSS test = new DSS(sk1, pk1);
-        BigInteger[] sig = test.sign(m);
-        System.out.println(test.verify(m, sig));
+        DSS user1 = new DSS(sk1, pk1);
+        BigInteger[] sig1 = user1.sign(m);
+
+        System.out.println("User 2 verify Sig1 from user 1:");
+
+        DSS user2_verify = new DSS(null, pk1);
+        user2_verify.verify(m, sig1);
+
+        System.out.println("User 2 verify Sig1 from user 1:");
+
+        m = new BigInteger(pk2.toString(16) + pk3.toString(16) + "00000001", 16);
+        System.out.println("m = 0x" + m.toString(16));
+
+        System.out.println("x = 0x" + sk2.toString(16));
+        System.out.println("y = 0x" + pk2.toString(16));
+
+        DSS user2_sign = new DSS(sk2, pk2);
+        BigInteger[] sig2 = user2_sign.sign(m);
 
         return;
     }
@@ -86,7 +94,7 @@ public class Main
 
             BigInteger h_m = new BigInteger(sha3_224(m.toString(16)), 16);
 
-            System.out.println("1. h_m = " + h_m.mod(q).toString());
+            System.out.println("1. h_m = 0x" + h_m.mod(q).toString(16));
 
             SecureRandom rnd = new SecureRandom();
             BigInteger k = new BigInteger(q.bitLength(), rnd);
@@ -95,13 +103,13 @@ public class Main
                 k = new BigInteger(q.bitLength(), rnd);
             }
 
-            System.out.println("2. k = " + k.toString());
+            System.out.println("2. k = 0x" + k.toString(16));
 
             BigInteger r = g.modPow(k, p).mod(q);
-            System.out.println("3. r = " + r.toString());
+            System.out.println("3. r = 0x" + r.toString(16));
 
             BigInteger s = h_m.subtract(x.multiply(r)).multiply(k.modInverse(q)).mod(q);
-            System.out.println("4. s = " + s.toString());
+            System.out.println("4. s = 0x" + s.toString(16));
 
             if (h_m.mod(q).equals(x.multiply(r).add(k.multiply(s)).mod(q))) {
                 System.out.println("5. signature is (" + r.toString() + ", " + s.toString() + ")");
@@ -126,25 +134,25 @@ public class Main
             }
 
             BigInteger h_m = new BigInteger(sha3_224(m.toString(16)), 16);
-            System.out.println("1. h_m = " + h_m.mod(q).toString());
+            System.out.println("1. h_m = 0x" + h_m.mod(q).toString());
 
             BigInteger r = sig[0];
             BigInteger s = sig[1];
 
             BigInteger s_inv = s.modInverse(q);
-            System.out.println("2. s^{-1} = " + s_inv.toString());
+            System.out.println("2. s^(-1) = 0x" + s_inv.toString());
 
             BigInteger u = h_m.multiply(s_inv).mod(q);
-            System.out.println("3. u = " + u.toString());
+            System.out.println("3. u = 0x" + u.toString());
 
             BigInteger v = r.negate().multiply(s_inv).mod(q);
-            System.out.println("4. v = " + v.toString());
+            System.out.println("4. v = 0x" + v.toString());
 
             BigInteger w = g.modPow(u, p).multiply(y.modPow(v, p)).mod(p).mod(q);
-            System.out.println("5. w = " + w.toString());
+            System.out.println("5. w = 0x" + w.toString());
 
             if (w.equals(r)) {
-                System.out.println("6. w = r = " + r.toString() + ". Verification success.");
+                System.out.println("Verification success.");
                 return true;
             }
 
--- MultiplicativeOrder.java
+++ MultiplicativeOrder.java
@@ -13,34 +13,34 @@ public class MultiplicativeOrder {
 	public static class PrimeExp {
 		BigInteger prime;
 		long exponent;
-		
+
 		PrimeExp(BigInteger prime, long exponent) {
 			this.prime = prime;
 			this.exponent = exponent;
 		}
 	}
-	
+
 	public static BigInteger findMultiplicativeInverse(BigInteger base, BigInteger modulo) {
 		// Factor  modulo - 1
 		BigInteger modulo1 = modulo.subtract(ONE);
 		List<PrimeExp> primeExpList = primeFactor(modulo1);
 		BigInteger multiOrder = ONE;
-		
+
 		// Fine the multiplicative inverse for each factor
 		for (PrimeExp primeExp : primeExpList) {
 			BigInteger counter = BigInteger.ZERO;
-			
+
 			// Divide modulo1 by one factor
 			BigInteger Mi = modulo1.divide(primeExp.prime.pow((int) primeExp.exponent));
 			BigInteger multiInv = base.modPow(Mi,modulo);
-			
+
 			// If it does not equate to 1, continue modPow with the factor until it's 1
 			while (multiInv.compareTo(ONE) > 0) {
 				multiInv = multiInv.modPow(primeExp.prime, modulo);
 				counter = counter.add(ONE);
 			}
 			System.out.println(counter);
-			
+
 			// No BigInteger Least Common Multiple. Use reduction by GCD
 			counter = primeExp.prime.pow(counter.intValue());
 			counter = counter.divide(multiOrder.gcd(counter));
@@ -48,28 +48,28 @@ public class MultiplicativeOrder {
 		}
 		return multiOrder;
 	}
-	
+
 	private static List<PrimeExp> primeFactor(BigInteger num) {
 		List<PrimeExp> primeExpList = new ArrayList<>();
 		BigInteger current = num; // Keep track as we factor
 		Long exp = 0L; // Exponent for factors
-		
+
 		// Check how many factors or 2 there are
 		while(!current.testBit(exp.intValue())) exp++;
 		current = current.shiftRight(exp.intValue());
-		
+
 		primeExpList.add(new PrimeExp(TWO, exp));
-		
+
 		// Factor for numbers larger than 2
 		BigInteger root = sqrt(current);
 		BigInteger factor = THREE;
-		
+
 		// Stop when current number reaches 1
 		while (current.compareTo(ONE) > 0) {
-			
+
 			// If the current factor is greater than the root, the root is the final factor
 			if (factor.compareTo(root) > 0) factor = current;
-			
+
 			// Keep dividing by factor until a remainder appears
 			exp = -1L;
 			BigInteger[] qr;
@@ -77,22 +77,22 @@ public class MultiplicativeOrder {
 				qr = current.divideAndRemainder(factor);
 				exp ++;
 			} while (qr[1].bitLength() <= 0);
-			
+
 			current = qr[0];
-			
+
 			// Add the prime factor and exponents to list
 			if (exp > 0L) {
 				primeExpList.add(new PrimeExp(factor, exp));
 				root = sqrt(current);
 			}
-			
+
 			// Increment factor by 2
 			factor = factor.add(TWO);
 		}
-		
+
 		return primeExpList;
 	}
-	
+
 	// Returns ceiling of square root
 	private static BigInteger sqrt(BigInteger num) {
 		BigInteger current = num;
